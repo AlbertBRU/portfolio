826ad1bc76ee31010aea7ac14c127502f66e94ad
Albert <albertbru01@gmail.com>
2023-12-21 20:49:39 +0100

    avancée en cours sur testimony

---
+++ src/components/Contact/TestimonyCards/TestimonyCard.jsx
@@ -0,0 +1,23 @@
+import './style.scss';
+
+export default function TestimonyCard({
+    name,
+    position,
+    testimony,
+}) {
+    return (
+        <div className='contact__carousel__card'>
+        <img
+            className="contact__carousel__card__image"
+            src={`/src/assets/images/contact/carousel/${name.toLowerCase()}.png`}
+        />
+        <div className="contact__carousel__card__container">
+            <div className="contact__carousel__card__container__name">{name}</div>
+            <div className="contact__carousel__card__container__position">{position}</div>
+            <div className="contact__carousel__card__container__text">
+                <p>{testimony}</p>
+            </div>
+        </div>
+    </div>
+    )
+}
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,5 +1,9 @@
 import ContactCard from './ContactCards/ContactCard';
+import { Carousel } from 'react-responsive-carousel';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import './style.scss';
+import TestimonyCard from './TestimonyCards/TestimonyCard';
 
 export default function Contact() {
     return (
@@ -18,9 +22,14 @@ export default function Contact() {
             <div className="contact__infos">
                 <p className='contact__infos__text'>Vous pouvez me contacter facilement en utilisant une des options ci-dessous :</p>
 
+                <button className="contact__infos__dl-button">
+                    <a href="/src/assets/images/contact/resume.pdf" download="CV-albert-bru">
+                        <img src='/src/assets/images/download-icon.png' />
+                        Télécharger mon CV au format PDF
+                    </a>
+                </button>
 
                 <div className="contact__infos__cards">
-                    <img src="/src/assets/images/contact/resume.png" alt="resume albert bru" className='contact__infos__resume' />
                     <ContactCard
                         link="linkedin.com/in/albertbrudigital"
                         buttonContent="Sur LinkedIn"
@@ -43,6 +52,34 @@ export default function Contact() {
                     />
                 </div>
             </div>
+
+            <h3>Ils parlent de moi...</h3>
+            <h4>... En bien (pour la plupart)</h4>
+
+            <Carousel
+                className="contact__carousel"
+                showArrows={true}
+                infiniteLoop={true}
+                showThumbs={false}
+                showStatus={false}
+                autoPlay={true}
+                interval={6600}
+            >
+
+                <TestimonyCard
+                    name="Louise"
+                    position="Ancienne collègue"
+                    testimony="J'ai eu l'opportunité de travailler avec Albert lors de son alternance en tant que Concepteur Développeur d'Applications au sein de l'École O'clock.
+                    C'était un vrai plaisir d'avoir une personne aussi impliquée et dynamique sur le projet, qui a toujours su être force de proposition quand je manquais d'idées et pédagogue lorsque je voulais mieux comprendre certains concepts techniques. Albert a fait preuve d'une excellente compréhension des enjeux et des exigences du projet et a su y répondre avec professionnalisme et créativité.
+                    Je recommande chaleureusement ce profil et serais enchantée de collaborer à nouveau avec lui."
+                />
+
+                <TestimonyCard
+                    name="Albert"
+                    position="Moi-même"
+                    testimony="Ce gars la, c'est un type bien. Il est sympa, il est drôle, il est beau, il est intelligent, il est modeste, il est humble, il est gentil, il est généreux, il est altruiste, il est serviable, il est courageux, il est chez lui, et il cherche du taff."
+                />
+            </Carousel>
         </div>
     );
 }
