b475dabfc281eff5a5546331e0744f7191f7e2fa
Albert <albertbru01@gmail.com>
2024-01-09 20:25:46 +0100

    changement du logo centrage des réalisations dans leur container

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,7 +3,7 @@ import './style.scss';
 export default function Menu() {
     return (
         <div className="header__menu">
-            <a className="invisible" href="#home">AB</a>
+            <a className="header__menu-logo invisible" href="#home">Albert&nbsp;Bru</a>
             <div className="header__menu__dummy-space invisible"></div>
             <a className="invisible" href="#home">Home</a>
             <a href="#realisations">Réalisations</a>
