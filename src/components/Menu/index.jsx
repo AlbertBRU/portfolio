29f4ffa2acaddc482cbd188902ea838a17959a9a
Albert <albertbru01@gmail.com>
2024-01-11 14:04:03 +0100

    gestion du burger menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import './style.scss';
 
 export default function Menu() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    const menuClass = isMenuOpen ? 'menu-open' : '';
+    const burgerClass = isMenuOpen ? 'burger-open' : '';
+
     return (
         <div className="header__menu">
             <a className="header__menu-logo invisible" href="#home">Albert&nbsp;Bru</a>
+
             <div className="header__menu__dummy-space invisible"></div>
-            <a className="invisible" href="#home">Home</a>
-            <a href="#realisations">Réalisations</a>
-            <a href="#parcours">Parcours</a>
-            <a href="#contact">Contact</a>
+
+            <a className={`invisible menu-home ${menuClass}`} href="#home" onClick={toggleMenu}> Accueil </a>
+            <a className={`menu-realisations ${menuClass}`} href="#realisations" onClick={toggleMenu}> Réalisations </a>
+            <a className={`menu-parcours ${menuClass}`} href="#parcours" onClick={toggleMenu}> Parcours </a>
+            <a className={`menu-contact ${menuClass}`} href="#contact" onClick={toggleMenu}> Contact </a>
+
+            <div className={`header__menu-burger invisible ${burgerClass}`} onClick={toggleMenu}>
+                <span className="header__menu-burger-line line-1"></span>
+                <span className="header__menu-burger-line line-2"></span>
+                <span className="header__menu-burger-line line-3"></span>
+            </div>
         </div>
     );
 }
